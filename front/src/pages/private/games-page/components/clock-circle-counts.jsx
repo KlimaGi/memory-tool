c4fd78a6b6117f5d26dd11f47f806e5ad49defed
Giedre <giedre.klima@gmail.com>
2022-11-24 12:27:53 +0200

    feat: cock game style correction

--- front/src/pages/private/games-page/components/clock-circle-counts.jsx
+++ front/src/pages/private/games-page/components/clock-circle-counts.jsx
@@ -1,23 +1,13 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Clock } from 'react-clock-styled';
-
-const Main = styled.div`
-//min-height: 100vh;
-// display:flex;
-// justify-content:center;
-// align-items: center;
-// flex-wrap: wrap;
-.clock{margin: 10px 150px;}
- `;
+import styles from './clock.module.scss';
 
 function ClockCircleCounts() {
   return (
-    <Main>
-      <div className="clock">
-        <Clock time="06:00:00" size="sm" border="" color="#000957" handleColor="#344CB7" />
-      </div>
-    </Main>
+
+    <div className={styles.clock}>
+      <Clock time="06:00:00" size="md" border="1px solid" color="#868bb1" handleColor="#344CB7" />
+    </div>
 
   );
 }
