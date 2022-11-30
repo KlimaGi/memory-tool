f212fd8af900ea3d96328190b9d89b662c2acfc2
Giedre <giedre.klima@gmail.com>
2022-11-30 12:40:42 +0200

    feat: add part of animated rule

--- front/src/pages/private/games-page/components/animated-toggle-content.jsx
+++ front/src/pages/private/games-page/components/animated-toggle-content.jsx
@@ -1,9 +1,11 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React, { useState } from 'react';
 import styles from './animated-toggle-content.module.scss';
 
 function AnimatedToggleContent() {
   const [active, setActive] = useState(false);
   const classActive = active ? `${styles.active}` : '';
+  const classBox = active ? `${styles.box}` : '';
 
   return (
     <div className={styles.navigation}>
@@ -14,9 +16,8 @@ function AnimatedToggleContent() {
         className={`${styles['menu-toggle']} ${classActive}`}
         role="button"
         tabIndex="0"
-      >
-        <div className="menu" />
-      </div>
+      />
+      <div className={`${styles.menu} ${classBox}`} />
     </div>
   );
 }
