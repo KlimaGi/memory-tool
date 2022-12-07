7b1d0263e5ac96dca70670e4ece169d6f216130b
Giedre <giedre.klima@gmail.com>
2022-12-07 13:35:14 +0200

    feat: add memory quiz logic

--- front/src/pages/private/games-page/components/short-memory-quiz.jsx
+++ front/src/pages/private/games-page/components/short-memory-quiz.jsx
@@ -1,14 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { generateNums } from '../functions/memory-quiz';
 import styles from '../common-components/clock.module.scss';
 
 function ShortMemoryQuiz() {
-  const [numbers, setNumbers] = useState([]);
+  const [numbers, setNumbers] = useState('');
+  const [numQuantity, setNumQuantity] = useState(4);
+  const numsRef = useRef();
+  const [show, setShow] = useState(true);
 
   const generate = () => {
-    const num = generateNums(4);
-    setNumbers(num);
+    const num = generateNums(numQuantity);
+    const numStringify = num.join('');
+    setNumbers(numStringify);
+    setShow(true);
   };
+
+  const checkCorrectness = () => {
+    if (numsRef.current.value === numbers) setNumQuantity(numQuantity + 1);
+    else setNumQuantity(numQuantity - 2);
+
+    generate();
+    numsRef.current.value = '';
+    setShow(true);
+  };
+
+  setTimeout(() => {
+    setShow(false);
+  }, (numQuantity * 1, 5) * 1000);
 
   return (
     <div className={styles.container}>
@@ -17,14 +35,21 @@ function ShortMemoryQuiz() {
         type="button"
         className={styles.button}
       >
-        Begin
-
+        Start
       </button>
-      <p>{numbers.map((num) => num)}</p>
-      <input placeholder="answer" />
+      {
+        show && <p>{numbers}</p>
+      }
+      <input
+        type="number"
+        placeholder="answer"
+        className={styles['input-label']}
+        ref={numsRef}
+      />
       <button
         type="button"
         className={styles.button}
+        onClick={checkCorrectness}
       >
         Submit
 
