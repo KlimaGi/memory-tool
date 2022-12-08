ab87069c5d74d46a18433ba8c6530bba747a66b0
Giedre <giedre.klima@gmail.com>
2022-12-08 12:45:42 +0200

    refactor: dateStr module

--- back/modules/dateStr.js
+++ back/modules/dateStr.js
@@ -1,26 +1,20 @@
+const dateFormatted = (arr) => {
+  const formateDateArr = arr.slice(0, 3).map((num) => {
+    if (num < 10) return ('0').concat(num);
+    return num;
+  });
+  return formateDateArr.join('-');
+};
+
 module.exports = {
 
   dateStrAddOne: (progressDate) => {
-    const dateFormatted = (arr) => {
-      const formateDateArr = arr.slice(0, 3).map((num) => {
-        if (num < 10) return ('0').concat(num);
-        return num;
-      });
-      return formateDateArr.join('-');
-    };
     const reviewDate = progressDate;
     reviewDate[1] = Number(reviewDate[1]) + 1;
     const result = dateFormatted(reviewDate);
     return result;
   },
   dateStr: (progressDate) => {
-    const dateFormatted = (arr) => {
-      const formateDateArr = arr.slice(0, 3).map((num) => {
-        if (num < 10) return ('0').concat(num);
-        return num;
-      });
-      return formateDateArr.join('-');
-    };
     const result = dateFormatted(progressDate);
     return result;
   },
